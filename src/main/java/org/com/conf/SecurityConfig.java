a9126b471510747b7038548c357dd131d9134857
g-boram <go_bo_ram@naver.com>
2025-02-04 23:59:27 +0900

    [ 권한 테스트 푸시 ]

--- src/main/java/org/com/conf/SecurityConfig.java
+++ src/main/java/org/com/conf/SecurityConfig.java
@@ -36,7 +36,9 @@ public class SecurityConfig {
                                 "/details/**",
                                 "/api/**",
                                 "/other/**",
-                                "/buy/**"
+                                "/buy/**",
+                                "/www/hicar.shop",
+                                "/hicar.shop"
                         ).permitAll() // 인증 없이 접근 가능한 경로
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
                 )
